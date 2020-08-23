7d0953fac00acfb12eb6a2361029cd5b223c5f97
Sandra Llanos <sllanos@bancolombia.com.co>
2020-08-23 16:43:09 -0500

    conts and let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,22 @@
+/**
+ * variables y constantes
+ */
+
+// var ya no se usa
+
+//const = son variables que no pueden ser reasignados
+//let = son variables que si pueden ser reasignados
+
+const nombre = 'Sandra';
+const apellido = 'llanos'; 
+
+let  valorDado = 5;
+valorDado = 4;
+
+console.log ( nombre, apellido, valorDado );
+
+if (true) {
+    let valorDado = 6;
+    console.log(valorDado);
+}
+console.log(valorDado);
