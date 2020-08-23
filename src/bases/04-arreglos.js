5b85204062fa217bb63ff920bd5e882271c7d640
Sandra Llanos <sllanos@bancolombia.com.co>
2020-08-23 16:43:49 -0500

    array

---
+++ src/bases/04-arreglos.js
@@ -0,0 +1,21 @@
+/** Arreglos en js
+ * Es una colección de información que se encuentran en una misma variable
+ */
+
+//  const arreglo = new Array(100); es poco frecuente su uso
+// se usa para cuando se quieren crear arreglos con un tamaño fijo
+
+const arreglo = [1,2,3,4];
+// arreglo.push(1);
+// arreglo.push(2);
+// arreglo.push(3);
+// arreglo.push(4);//no es recomendable usar push pues modifica el objeto principal
+//se recomienda el spred
+
+const arreglo2 = [...arreglo, 5]
+const arreglo3 = arreglo2.map(function(num){
+    return num * 2;
+})
+console.log( arreglo );
+console.log( arreglo2 );
+console.log( arreglo3 );
